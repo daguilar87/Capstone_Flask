5b35492cf985f410ef9ee386066765278f326f76
Daniel Aguilar <daguilar8749@gmail.com>
2025-03-11 20:34:57 -0700

    updated database

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,11 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_moment import Moment
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 
 app = Flask(__name__)
 CORS(app)
